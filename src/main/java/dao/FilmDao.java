99c97fcea8a7065dc37853d9c31f1e31910e2b9d
Kylian Barthelemot <kylian.barthelemot@student.yncrea.fr>
2020-11-10 22:03:39 +0100

    Film DAO implementation

--- src/main/java/dao/FilmDao.java
+++ src/main/java/dao/FilmDao.java
@@ -1,5 +1,13 @@
 package dao;
 
+import java.util.List;
+
+import entity.Film;
+
 public interface FilmDao {
 
+	public List<Film> listFilms();
+	public Film getFilm(Integer id);
+	public Film addFilm(Film film);
+	
 }
--- src/main/java/dao/impl/FilmDaoImpl.java
+++ src/main/java/dao/impl/FilmDaoImpl.java
@@ -1,7 +1,28 @@
 package dao.impl;
 
+import java.util.List;
+
 import dao.FilmDao;
+import entity.Film;
 
 public class FilmDaoImpl implements FilmDao {
+
+	@Override
+	public List<Film> listFilms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film getFilm(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Film addFilm(Film film) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
