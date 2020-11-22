d4241588b78269fcc076db2c08d99edacdd3c60e
Quentin Callens <quentin.callens@hei.yncrea.fr>
2020-11-22 13:00:25 +0100

    Modif AddFilmServlet + ajoutFilm

--- src/main/webapp/js/view_it.js
+++ src/main/webapp/js/view_it.js
@@ -4,3 +4,13 @@ window.onload = function () {
     document.getElementById("header_ajoutfilm").hidden = true;*/
 
 };
+var fd = new FormData();
+fd.append('file', document.getElementById("image-addFilm").files[0]);
+var req;
+if (window.ActiveXObject) {
+    req=new ActiveXObject();
+} else {
+    req=new XMLHttpRequest();
+}
+req.open("post", "../user/ajoutfilm", true);
+req.send(fd);
